1e3258ef16de494150496484e8c970a3457e82d6
KunalChandraPoddar <141207461+KunalChandraPoddar@users.noreply.github.com>
2023-08-02 11:06:33 +0530

    Add files via upload

---
+++ bootcamp.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n;
+	cout<<"Enter the number to check wheather it is an even or an odd number= ";
+	cin>>n;
+	if (n%2==0)
+	cout<<"Number is even.";
+	else
+	cout<<"Number is odd.";
+	return 0;
+}
+
